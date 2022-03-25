f7d5431755bc37fe8b9a6f2bcbc0967d65a33cab
jasimuddin <jasimuddin25635@gmail.com>
2022-03-26 00:11:54 +0600

    now start project

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Shop.css";
+const Shop = () => {
+  return (
+    <div className="main-container">
+      <div className="shop-container">
+        <h1>product</h1>
+      </div>
+      <div className="cart-container">
+        <h1>cart summary</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
